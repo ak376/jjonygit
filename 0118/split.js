08efd501d08fe7ef1c5502476b139941381c3ccd
unknown <real980312@gmail.com>
2023-01-18 14:08:45 +0900

    queryseletor이용예시

---
+++ 0118/split.js
@@ -0,0 +1,57 @@
+// const a = "치킨|삼겹살|마라탕";
+// const array = a.split("|");
+// console.log(array);
+
+
+
+// ti = document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(1) > div.box-contents > a > strong");
+// per =document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(1) > div.box-contents > div > strong");
+// date =document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(1) > div.box-contents > span.txt-info > strong");
+
+// console.log(ti.textContent,per.textContent,date.textContent);
+
+// sti = document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(2) > div.box-contents > a > strong");
+// sper =document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(1) > div.box-contents > div > strong");
+// sdate =document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(2) > div.box-contents > span.txt-info > strong");
+
+// per.textContent = "예매율 5%";
+// sti.textContent ="삽겹살과 그대라면";
+// sdate.textContent = "1985.07.07 개봉"
+
+// firimg = document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(1) > div.box-image > a > span > img");
+// let empty;
+// thirimg = document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(3) > div.box-image > a > span > img");
+
+// empty=firimg.src;
+// firimg.src = thirimg.src;
+// thirimg.src=empty;
+
+// firurl= document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(1) > div.box-contents > span.like > a");
+// thirurl = document.querySelector("#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(2) > li:nth-child(3) > div.box-contents > span.like > a");
+
+// empty=firurl;
+// firurl = thirurl;
+// thirurl=empty;
+
+// const a= document.querySelectorAll(".box-contents strong");
+// for(let i =0; i<a.length; i++){
+  
+  
+//   console.log(a[i].textContent);
+  
+// }
+const b= document.querySelectorAll("input");
+for(let i =0; i<b.length; i++){
+  
+  
+  console.log(b[i].checked);
+  
+}
+// const a= document.querySelectorAll(".title");
+// const b= document.querySelectorAll(".percent>span");
+// for(let i =0; i<a.length; i++){
+//   a[i].textContent="*"
+//   b[i].textContent = "30%"
+//   console.log(a[i].textContent);
+//   console.log(b[i].textContent);
+// }
